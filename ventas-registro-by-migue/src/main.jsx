531094ae67bd1afa8ab448b70f0f7c0cf233e9fd
Migue2210 <pazhucatoursoperaciones@gmail.com>
2025-05-24 17:32:09 -0500

    Delete ventas-registro-by-migue directory

--- ventas-registro-by-migue/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ReporteVentas from './pages/ReporteVentas';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ReporteVentas />
-  </React.StrictMode>
-);
